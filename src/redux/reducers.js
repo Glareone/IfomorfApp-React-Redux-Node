ad816735e305cf6d43bb4ec9cc32a8e37dacf78a
Aliaksei Kalesnikau <aliaksei.kalesnikau@sorted.com>
2019-04-24 14:09:41 +0300

    redux, store, thunk

---
+++ src/redux/reducers.js
@@ -0,0 +1,13 @@
+const defaultState = {
+  isFetching: false,
+  apps: [],
+};
+
+function apps( state = defaultState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default apps
