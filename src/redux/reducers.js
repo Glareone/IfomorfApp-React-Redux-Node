96ded33ee3e26ef6ebcff901b4b5b49dbb44be8f
Aliaksei Kalesnikau <aliaksei.kalesnikau@sorted.com>
2019-04-24 15:35:58 +0300

    actions, reducers, components

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,3 +1,5 @@
+import { REQUEST_APPS,  RECEIVE_APPS } from './actions';
+
 const defaultState = {
   isFetching: false,
   apps: [],
@@ -5,6 +7,15 @@ const defaultState = {
 
 function apps( state = defaultState, action) {
   switch (action.type) {
+    case REQUEST_APPS:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case RECEIVE_APPS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        apps: action.apps
+      });
     default:
       return state
   }
